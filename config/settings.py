4b9abaedd9994917c0946017bd14fb46041a7d9f
Nagaeva Elina <05batman@mail.ru>
2025-05-09 22:17:18 +0400

    fixes from flake8

--- config/settings.py
+++ config/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": os.getenv("POSTGRES_DB", "project3"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "5432"),
+        "HOST": os.getenv("POSTGRES_HOST", "5432"),
+        "PORT": os.getenv("POSTGRES_PORT", "127.0.0.1"),
     }
 }
 
